be8eb96da7693f83e714a8595e1bb77b55a30a88
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-08-17 21:54:15 -0300

    Create shopmain.c
    Estava programando antes de utilizar o git então nesse momento estou passando o arquivo de onde eu parei

---
+++ shopmain.c
@@ -0,0 +1,66 @@
+/*
+UFRN - Universidade Federal do Rio Grande do Norte
+CERES - Centro de Ensino Superior do Seridó
+BSI - Bacharelado em Sistemas de Informação
+Developed by @SoutoCB - since august,2023
+DCT1106 - Programação - Flavius Gorgônio
+----------------------------------------
+Referencias;
+-Slides e Github de Flavius Gorgônio, @FlaviusGorgonio
+-Chat GPT, https://chat.openai.com/
+-Outras referencias, serão referenciadas no próprio codigo
+*/
+
+
+//Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+//Assinaturas das funções
+void tela_menu_principal(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+
+//Parte principal do programa
+
+int main()
+{
+    /* code */
+    return 0;
+}
+
+
+
+//Funções do sistema
+void tela_menu_principal(void) {
+    printf("Menu Principal\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Gestão do SexShop = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Cliente                                            ///\n");
+    printf("///            2. Módulo Funcionarios                                       ///\n");
+    printf("///            3. Módulo Gestão de Produtos                                 ///\n");
+    printf("///            4. Módulo Vendas e pedidos                                   ///\n");
+    printf("///            5. Módulo Relatórios                                         ///\n");
+    printf("///            6. Sobre                                                     ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Escolha a opção desejada: ");
+
+}
+
+void tela_sobre(void) {
+    printf("Menu Sobre\n");
+
+}
+
+void tela_equipe(void) {
+    printf("Menu equipe\n");
+
+}
+
