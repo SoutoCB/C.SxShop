8fbf944d3cecf4bbe40c9c4d24046eca904ab1b7
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-08-18 22:51:27 -0300

    Adcionando mais printf, no entando desorganizado

--- shopmain.c
+++ shopmain.c
@@ -30,6 +30,10 @@ void tela_equipe(void);
 int main()
 {
     /* code */
+    tela_menu_principal();
+    tela_sobre();
+    tela_equipe();
+
     return 0;
 }
 
@@ -42,7 +46,7 @@ void tela_menu_principal(void) {
     printf("///                                                                         ///\n");
     printf("///            = = = = = Sistema de Gestão do SexShop = = = = =             ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Módulo Cliente                                            ///\n");
+    printf("///            1. Modulo Cliente                                            ///\n");
     printf("///            2. Módulo Funcionarios                                       ///\n");
     printf("///            3. Módulo Gestão de Produtos                                 ///\n");
     printf("///            4. Módulo Vendas e pedidos                                   ///\n");
@@ -50,17 +54,28 @@ void tela_menu_principal(void) {
     printf("///            6. Sobre                                                     ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
-    printf("///            Escolha a opção desejada: ");
+    printf("///            Escolha a opção desejada: \n");
 
 }
 
 void tela_sobre(void) {
     printf("Menu Sobre\n");
+    printf("                          -Sistema de Gestão de um SexShop-\n");
+    printf("Esse projeto tem como objetivo a conclusão de DCT1106 - Programação, ministrada por Flavius Gorgônio\n");
+    printf("se trata de um sistema de gestão para um sexshop, que visa conter todos os requisitos necessarios para ser avaliado\n");
+    printf("desenvolvido por @SoutoCB, discente em Bacharelado em Sistemas de Informação - UFRN CERES.\n");
+    printf("base utilizada para desenvolver o projeto está localizada nesse endereço github: https://github.com/FlaviusGorgonio/LinguaSolta.git\n ");
+
+
 
 }
 
 void tela_equipe(void) {
     printf("Menu equipe\n");
+    printf("Esse projeto foi desenvolvido por;\n");
+    printf("Discente: Bruno Costa Souto\n");
+    printf("GitHub: @SoutoCB\n");
+    printf("Endereço: https://github.com/SoutoCB/C.SxShop.git\n ");
 
 }
 
