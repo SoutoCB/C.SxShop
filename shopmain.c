4e80aa899a1a370004db19d3db4221a92ae698ac
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-08-19 22:02:30 -0300

    Permitindo Caracteres especiais
    Utilizei a biblioteca locale.h, para resolver o problema com os caracteres especiais na hora do compilamento.

--- shopmain.c
+++ shopmain.c
@@ -1,25 +1,26 @@
 /*
 UFRN - Universidade Federal do Rio Grande do Norte
-CERES - Centro de Ensino Superior do Serid√≥
-BSI - Bacharelado em Sistemas de Informa√ß√£o
+CERES - Centro de Ensino Superior do SeridÛ
+BSI - Bacharelado em Sistemas de InformaÁ„o
 Developed by @SoutoCB - since august,2023
-DCT1106 - Programa√ß√£o - Flavius Gorg√¥nio
+DCT1106 - ProgramaÁ„o - Flavius GorgÙnio
 ----------------------------------------
 Referencias;
--Slides e Github de Flavius Gorg√¥nio, @FlaviusGorgonio
+-Slides e Github de Flavius GorgÙnio, @FlaviusGorgonio
 -Chat GPT, https://chat.openai.com/
--Outras referencias, ser√£o referenciadas no pr√≥prio codigo
+-Outras referencias, ser„o referenciadas no prÛprio codigo
 */
 
 
 //Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 
 
 
-//Assinaturas das fun√ß√µes
+//Assinaturas das funÁıes
 void tela_menu_principal(void);
 void tela_sobre(void);
 void tela_equipe(void);
@@ -30,6 +31,8 @@ void tela_equipe(void);
 int main()
 {
     /* code */
+    ///FunÁ„o para permitir caracteres especiais
+    setlocale(LC_ALL,"Portuguese_Brazil");
     tela_menu_principal();
     tela_sobre();
     tela_equipe();
@@ -39,32 +42,32 @@ int main()
 
 
 
-//Fun√ß√µes do sistema
+//FunÁıes do sistema
 void tela_menu_principal(void) {
     printf("Menu Principal\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///            = = = = = Sistema de Gest√£o do SexShop = = = = =             ///\n");
+    printf("///            = = = = = Sistema de Gest„o do SexShop = = = = =             ///\n");
     printf("///                                                                         ///\n");
     printf("///            1. Modulo Cliente                                            ///\n");
-    printf("///            2. M√≥dulo Funcionarios                                       ///\n");
-    printf("///            3. M√≥dulo Gest√£o de Produtos                                 ///\n");
-    printf("///            4. M√≥dulo Vendas e pedidos                                   ///\n");
-    printf("///            5. M√≥dulo Relat√≥rios                                         ///\n");
+    printf("///            2. MÛdulo Funcionarios                                       ///\n");
+    printf("///            3. MÛdulo Gest„o de Produtos                                 ///\n");
+    printf("///            4. MÛdulo Vendas e pedidos                                   ///\n");
+    printf("///            5. MÛdulo RelatÛrios                                         ///\n");
     printf("///            6. Sobre                                                     ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
-    printf("///            Escolha a op√ß√£o desejada: \n");
+    printf("///            Escolha a opÁ„o desejada: \n");
 
 }
 
 void tela_sobre(void) {
     printf("Menu Sobre\n");
-    printf("                          -Sistema de Gest√£o de um SexShop-\n");
-    printf("Esse projeto tem como objetivo a conclus√£o de DCT1106 - Programa√ß√£o, ministrada por Flavius Gorg√¥nio\n");
-    printf("se trata de um sistema de gest√£o para um sexshop, que visa conter todos os requisitos necessarios para ser avaliado\n");
-    printf("desenvolvido por @SoutoCB, discente em Bacharelado em Sistemas de Informa√ß√£o - UFRN CERES.\n");
-    printf("base utilizada para desenvolver o projeto est√° localizada nesse endere√ßo github: https://github.com/FlaviusGorgonio/LinguaSolta.git\n ");
+    printf("                          -Sistema de Gest„o de um SexShop-\n");
+    printf("Esse projeto tem como objetivo a conclus„o de DCT1106 - ProgramaÁ„o, ministrada por Flavius GorgÙnio\n");
+    printf("se trata de um sistema de gest„o para um sexshop, que visa conter todos os requisitos necessarios para ser avaliado\n");
+    printf("desenvolvido por @SoutoCB, discente em Bacharelado em Sistemas de InformaÁ„o - UFRN CERES.\n"); 
+    printf("base utilizada para desenvolver o projeto est· localizada nesse endereÁo github: https://github.com/FlaviusGorgonio/LinguaSolta.git\n ");
 
 
 
@@ -75,7 +78,7 @@ void tela_equipe(void) {
     printf("Esse projeto foi desenvolvido por;\n");
     printf("Discente: Bruno Costa Souto\n");
     printf("GitHub: @SoutoCB\n");
-    printf("Endere√ßo: https://github.com/SoutoCB/C.SxShop.git\n ");
+    printf("EndereÁo: https://github.com/SoutoCB/C.SxShop.git\n ");
 
 }
 
