62e5620b55021ee66ffa6e11dce81c8c43a943ec
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-08-20 16:10:23 -0300

    Adição dos menus do menu principal
    Apoio do ChatGPT para agilizar o processo

--- shopmain.c
+++ shopmain.c
@@ -22,8 +22,14 @@ Referencias;
 
 //Assinaturas das funções
 void tela_menu_principal(void);
+void tela_menu_cliente(void);
+void tela_menu_funcionarios(void);
+void tela_menu_gestao_produtos(void);
+void tela_menu_vendas_pedidos(void);
+void tela_menu_relatorios(void);
 void tela_sobre(void);
 void tela_equipe(void);
+
 
 
 //Parte principal do programa
@@ -34,6 +40,11 @@ int main()
     ///Função para permitir caracteres especiais
     setlocale(LC_ALL,"Portuguese_Brazil");
     tela_menu_principal();
+    tela_menu_cliente();
+    tela_menu_funcionarios();
+    tela_menu_gestao_produtos();
+    tela_menu_vendas_pedidos();
+    tela_menu_relatorios();
     tela_sobre();
     tela_equipe();
 
@@ -61,6 +72,86 @@ void tela_menu_principal(void) {
     printf("|===============================================================================|\n\n");
 
 }
+void tela_menu_cliente(void) {
+    printf("Menu Cliente\n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Menu Cliente = = = = =                                   |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Cadastrar Cliente                                               |\n");
+    printf("|            2. Listar Clientes                                                 |\n");
+    printf("|            3. Editar Cliente                                                  |\n");
+    printf("|            4. Excluir Cliente                                                 |\n");
+    printf("|            0. Voltar ao Menu Principal                                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
+}
+
+void tela_menu_funcionarios(void) {
+    printf("Menu Funcionários\n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Menu Funcionários = = = = =                              |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Cadastrar Funcionário                                           |\n");
+    printf("|            2. Listar Funcionários                                             |\n");
+    printf("|            3. Editar Funcionário                                              |\n");
+    printf("|            4. Excluir Funcionário                                             |\n");
+    printf("|            0. Voltar ao Menu Principal                                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
+}
+
+void tela_menu_gestao_produtos(void) {
+    printf("Menu Gestão de Produtos\n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Menu Gestão de Produtos = = = = =                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Cadastrar Produto                                               |\n");
+    printf("|            2. Listar Produtos                                                 |\n");
+    printf("|            3. Editar Produto                                                  |\n");
+    printf("|            4. Excluir Produto                                                 |\n");
+    printf("|            0. Voltar ao Menu Principal                                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
+}
+
+void tela_menu_vendas_pedidos(void) {
+    printf("Menu Vendas e Pedidos\n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Menu Vendas e Pedidos = = = = =                          |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Realizar Venda                                                  |\n");
+    printf("|            2. Listar Pedidos                                                  |\n");
+    printf("|            3. Editar Pedido                                                   |\n");
+    printf("|            4. Cancelar Pedido                                                 |\n");
+    printf("|            0. Voltar ao Menu Principal                                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
+}
+
+void tela_menu_relatorios(void) {
+    printf("Menu Relatórios\n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Menu Relatórios = = = = =                                |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Relatório de Clientes                                           |\n");
+    printf("|            2. Relatório de Funcionários                                       |\n");
+    printf("|            3. Relatório de Produtos                                           |\n");
+    printf("|            4. Relatório de Vendas                                             |\n");
+    printf("|            0. Voltar ao Menu Principal                                        |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
+}
+
 
 void tela_sobre(void) {
     printf("Menu Sobre\n");
