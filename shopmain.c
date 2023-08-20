3dc2aa2b89ea41ca09561e87e9b3f322a7438256
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-08-20 15:56:46 -0300

    Formatando os menus ja existentes

--- shopmain.c
+++ shopmain.c
@@ -45,29 +45,36 @@ int main()
 //Funções do sistema
 void tela_menu_principal(void) {
     printf("Menu Principal\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = Sistema de Gestão do SexShop = = = = =             ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Modulo Cliente                                            ///\n");
-    printf("///            2. Módulo Funcionarios                                       ///\n");
-    printf("///            3. Módulo Gestão de Produtos                                 ///\n");
-    printf("///            4. Módulo Vendas e pedidos                                   ///\n");
-    printf("///            5. Módulo Relatórios                                         ///\n");
-    printf("///            6. Sobre                                                     ///\n");
-    printf("///            0. Sair                                                      ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Escolha a opção desejada: \n");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Sistema de Gestão do SexShop = = = = =                   |\n");
+    printf("|                                                                               |\n");
+    printf("|            1. Menu Cliente                                                    |\n");
+    printf("|            2. Menu Funcionarios                                               |\n");
+    printf("|            3. Menu Gestão de Produtos                                         |\n");
+    printf("|            4. Menu Vendas e pedidos                                           |\n");
+    printf("|            5. Menu Relatórios                                                 |\n");
+    printf("|            6. Sobre                                                           |\n");
+    printf("|            0. Sair                                                            |\n");
+    printf("|                                                                               |\n");
+    printf("|            Escolha a opção desejada:                                          |\n"); 
+    printf("|===============================================================================|\n\n");
 
 }
 
 void tela_sobre(void) {
     printf("Menu Sobre\n");
-    printf("                          -Sistema de Gestão de um SexShop-\n");
-    printf("Esse projeto tem como objetivo a conclusão de DCT1106 - Programação, ministrada por Flavius Gorgônio\n");
-    printf("se trata de um sistema de gestão para um sexshop, que visa conter todos os requisitos necessarios para ser avaliado\n");
-    printf("desenvolvido por @SoutoCB, discente em Bacharelado em Sistemas de Informação - UFRN CERES.\n"); 
-    printf("base utilizada para desenvolver o projeto está localizada nesse endereço github: https://github.com/FlaviusGorgonio/LinguaSolta.git\n ");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Sistema de Gestão do SexShop = = = = =                   |\n");
+    printf("|                                                                               |\n");
+    printf("|Esse projeto tem como objetivo a conclusão de DCT1106 - Programação, ministrada|\n");
+    printf("|por Flavius Gorgônio, @FlaviusGorgonio. Se trata de um sistema de gestão para  |\n");
+    printf("|um SexShop, que visa conter todos os requisitos necessários para ser avaliado. |\n");
+    printf("|Desenvolvido por @SoutoCB, discente em Bacharelado em Sistemas de informação - |\n");
+    printf("|UFRN CERES. Base utilizada para desenvolver o projeto está localizada em:      |\n"); 
+    printf("|https://github.com/FlaviusGorgonio/LinguaSolta.git                             |\n");
+    printf("|===============================================================================|\n\n");
 
 
 
@@ -75,10 +82,15 @@ void tela_sobre(void) {
 
 void tela_equipe(void) {
     printf("Menu equipe\n");
-    printf("Esse projeto foi desenvolvido por;\n");
-    printf("Discente: Bruno Costa Souto\n");
-    printf("GitHub: @SoutoCB\n");
-    printf("Endereço: https://github.com/SoutoCB/C.SxShop.git\n ");
+    printf("|===============================================================================|\n");
+    printf("|                                                                               |\n");
+    printf("|            = = = = = Sistema de Gestão do SexShop = = = = =                   |\n");
+    printf("|                                                                               |\n");
+    printf("|Esse projeto foi desenvolvido por;                                             |\n");
+    printf("|Discente: Bruno Costa Souto                                                    |\n");
+    printf("|GitHub: @SoutoCB                                                               |\n");
+    printf("|Endereço: https://github.com/SoutoCB/C.SxShop.git                              |\n");
+    printf("|===============================================================================|\n\n");
 
 }
 
