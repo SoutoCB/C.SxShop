89e6c46f13ee6fa15fb6151277c454bc5198a33f
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-09-15 18:49:34 -0300

    feat
    adcionando modularização para o modulo cliente

---
+++ cliente.h
@@ -0,0 +1,9 @@
+
+//Assinaturas modulo clientes
+
+
+void tela_menu_cliente();
+    void cadast_cliente(void);
+    void exibir_cliente(void);
+    void edit_cliente(void);
+    void delet_cliente(void);
