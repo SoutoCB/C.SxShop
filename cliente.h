13ca46cbfefe655d60137fd170f372abccc38681
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-10-15 21:19:29 -0300

    feat
    struct para cliente, inicio

--- cliente.h
+++ cliente.h
@@ -1,6 +1,12 @@
 
 //Assinaturas modulo clientes
-
+struct cliente{
+    char codigoc[10];
+    char cpfc[12]; 
+    char nomec[50];
+    char data_nascimentoc[11]; // "dd/mm/aaaa"
+    char telefonec[12];
+};
 
 void tela_menu_cliente();
     void cadast_cliente(void);
