4c62ff01930a4613c67e2c19bfd1d23ce0c391c3
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-11-25 17:52:45 -0300

    feat
    inciando uma novo relatorio

--- relatorios.c
+++ relatorios.c
@@ -163,6 +163,53 @@ void cliente_p_situacao(void) {
     }
 }
 
+/*
+void numero_v_cliente(void) {
+    char cpf[12];
+    printf("Digite o cpf do cliente: ");
+    do{le_cpf(cpf)}while(!verificacpf);
+
+    if(op == '1' || op == '2'){
+        system("clear || cls");  // Tenta "clear" no Linux/macOS, se falhar, tenta "cls" no Windows
+        FILE* fp;
+        Cliente* cliente;
+        cliente = (Cliente*) malloc(sizeof(Cliente));
+        printf("|\033[1;36m = Lista de clientes = \033[0m|\n");
+        printf("|===============================================================================|\n");
+        printf("|                                                                               |\n");
+        printf("|                      = = = = = Menu Cliente = = = = =                         |\n");
+        printf("|                                                                               |\n");
+        printf("|   CODIGO  |    CPF     |              NOME            |      SITUACAO         |\n");            
+        printf("|-------------------------------------------------------------------------------|\n");
+        fp = fopen("clientes.dat", "rb");
+        if (fp == NULL) {
+            printf("Erro na abertura do arquivo.\n");
+            printf("Nao e possivel continuar, provavelmente nao tem clientes cadastrados...\n");
+            exit(1);
+        }
+        if (op == '1'){
+            while(fread(cliente, sizeof(Cliente), 1, fp)) {
+                if (cliente->status == 'a') {
+                    exibir_clientert(cliente);
+                }
+            }
+        } else if (op == '2'){
+            while(fread(cliente, sizeof(Cliente), 1, fp)) {
+                if (cliente->status == 'x') {
+                    exibir_clientert(cliente);
+                }
+            }
+        }
+        free(cliente);
+        fclose(fp);
+    } else{
+        printf("Opcao invalida.\n");
+    }
+}
+
+
+*/
+
 
 void relat_funcionarios(void) {
     char op1;
