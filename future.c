890071b3084409fef88a35535b410c4a65db0bd1
Bruno Costa Souto <142504373+SoutoCB@users.noreply.github.com>
2023-11-30 20:41:49 -0300

    feat, style
    lembretes concluídos e melhora no future.c

--- future.c
+++ future.c
@@ -1,48 +1,48 @@
-//Destinada a futuras adicoes
+/*
+Destinada a futuras adicoes
 
-//Uso de malloc, para otimizacao de espaco
+Uso de malloc, para otimizacao de espaco
 
-//Lembrar de sempre testar no ambiente linux, Replit.
-//replit:
-//gcc -Wall *.h -c
-//gcc -Wall *.c -c 
-//gcc -Wall *.c -o testec
-//./testec
+Lembrar de sempre testar no ambiente linux, Replit.
+replit:
+gcc -Wall *.h -c
+gcc -Wall *.c -c 
+gcc -Wall *.c -o testec
+./testec
 
-//Colocar para exibir o cadastros excluidos ou inativos, principalmente nos relatorios.
-//Opcao, caso o cpf já tenha sido cadastrado antes, de poder recuperar cadastro
-//Exibir os produtos esgotados no menu de gestão, como um aviso
-//Reajusta quando escreve as resposta, para responder do lado da pergunta
-//Filtro envolvendo 3 modulos
-    //Utimas 10 vendas.
-    //Numero de vendas por produto
-    //Produtos comprados por cliente
-    //Clientes que compraram determinado produto
-    //Vendas detalhadas
-//Colocar filtro de ordem alfabetica
+Colocar para exibir o cadastros excluidos ou inativos, principalmente nos relatorios.
+Opcao, caso o cpf já tenha sido cadastrado antes, de poder recuperar cadastro
+Exibir os produtos esgotados no menu de gestão, como um aviso
+Reajusta quando escreve as resposta, para responder do lado da pergunta
+Filtro envolvendo 3 modulos
+    Utimas 10 vendas.
+    Numero de vendas por produto
+    Vendas detalhadas
+    Colocar filtro de ordem alfabetica
 
-                                  // Semana 9 slides Flavius
+                                   Semana 9 slides Flavius
                                 
-// Tabela ASCII
+Tabela ASCII
 
-//Algumas funções da biblioteca <string.h>
-// Copia o conteúdo da string2 na string1
-//char *strcpy (char *dest, const char *src);
+Algumas funções da biblioteca <string.h>
+ Copia o conteúdo da string2 na string1
+char *strcpy (char *dest, const char *src);
 
-// Copia os n primeiros caracteres da string2 na string1
-//char *strncpy(char *string1, const char *string2, size_t n);
+ Copia os n primeiros caracteres da string2 na string1
+char *strncpy(char *string1, const char *string2, size_t n);
 
-// Concatena a string2 no final da string1
-//char *strcat(char *string1, const char *string2);
+ Concatena a string2 no final da string1
+char *strcat(char *string1, const char *string2);
 
-// Concatena n caracteres da string2 no final da string1
-//char *strncat(char *string1, const char *string2, size_t n);
+ Concatena n caracteres da string2 no final da string1
+char *strncat(char *string1, const char *string2, size_t n);
 
-// Compara duas strings e determina a ordem (alfabética) das duas
-//int strcmp(const char *string1, const char *string2);
+ Compara duas strings e determina a ordem (alfabética) das duas
+int strcmp(const char *string1, const char *string2);
 
-// Compara os n primeiros caracteres de duas strings
-//int strncmp(const char *string1, char *string2, size_t n);
+ Compara os n primeiros caracteres de duas strings
+int strncmp(const char *string1, char *string2, size_t n);
 
-// Retorna o tamanho de uma string
-//int strlen(const char *string);
+ Retorna o tamanho de uma string
+int strlen(const char *string);
+*/
